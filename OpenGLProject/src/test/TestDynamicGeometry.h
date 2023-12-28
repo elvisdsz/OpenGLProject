52822ff5ac2e80e802a1efaa4aaa74cda37b31ff
Elvis Dsouza <elvis.dsouza666@gmail.com>
2023-12-28 20:32:00 +0000

    Duplicate class for dyanmic geometry test

---
+++ OpenGLProject/src/test/TestDynamicGeometry.h
@@ -0,0 +1,34 @@
+#pragma once
+
+#include "Test.h"
+#include "Texture.h"
+#include "VertexArray.h"
+#include "glm/vec3.hpp"
+#include "imgui/imgui.h"
+
+namespace test
+{
+	class TestDynamicGeometry : public Test
+	{
+	public:
+		TestDynamicGeometry();
+		~TestDynamicGeometry();
+
+		void OnUpdate(float deltaTime) override;
+		void OnRender() override;
+		void OnImGuiRender() override;
+
+	private:
+		std::unique_ptr<VertexArray> m_VAO;
+		std::unique_ptr<VertexBuffer> m_VertexBuffer;
+		std::unique_ptr<IndexBuffer> m_IndexBuffer;
+		std::unique_ptr<Shader> m_Shader;
+		std::unique_ptr<Texture> m_Texture1, m_Texture2;
+
+		glm::mat4 m_Proj, m_View;
+		glm::vec3 m_TranslationA, m_TranslationB;
+
+		ImGuiIO& m_IO;
+
+	};
+}
