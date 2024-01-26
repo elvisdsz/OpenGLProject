4b21ac3b6539eead6773cac59243f86a4dc7e8ac
Elvis Dsouza <elvis.dsouza666@gmail.com>
2024-01-26 22:30:39 +0000

    Camera rotation functionality using yaw/pitch offsets

--- OpenGLProject/src/Camera.cpp
+++ OpenGLProject/src/Camera.cpp
@@ -4,10 +4,11 @@
 #include "glm/ext/matrix_transform.hpp"
 
 Camera::Camera(glm::vec3 position, float width, float height)
-	: m_Position(position), m_Width(width), m_Height(height), m_AspectRatio(width/height), m_ZoomUnits(0.0f)
+	: m_Position(position), m_Width(width), m_Height(height), m_AspectRatio(width/height), m_ZoomUnits(0.0f),
+	m_Orientation(glm::vec3(0.0f, 0.0f, -1.0f))
 {
 	const glm::mat4 viewMatrix = glm::lookAt(m_Position, m_Position + glm::vec3(0.0f, 0.0f, -1.0f), glm::vec3(0.0f, 1.0f, 0.0f));
-	const glm::mat4 projMatrix = glm::ortho(0.0f, m_Width, 0.0f, m_Height, -1.0f, 1.0f);
+	const glm::mat4 projMatrix = glm::ortho(0.0f, m_Width, 0.0f, m_Height, Z_NEAR, Z_FAR);
 	m_CamMatrix = projMatrix * viewMatrix;
 }
 
@@ -53,11 +54,48 @@ void Camera::OnUpdate(GLFWwindow* window)
 		modified = true;
 	}
 
+	bool orientationChanged = false;
+	// Orientation
+	if (glfwGetKey(window, GLFW_KEY_RIGHT) == GLFW_PRESS)
+	{
+		m_Yaw += m_Sensitivity;
+		orientationChanged = true;
+	}
+	if (glfwGetKey(window, GLFW_KEY_LEFT) == GLFW_PRESS)
+	{
+		m_Yaw -= m_Sensitivity;
+		orientationChanged = true;
+	}
+	if (glfwGetKey(window, GLFW_KEY_UP) == GLFW_PRESS)
+	{
+		m_Pitch -= m_Sensitivity;
+		orientationChanged = true;
+	}
+	if (glfwGetKey(window, GLFW_KEY_DOWN) == GLFW_PRESS)
+	{
+		m_Pitch += m_Sensitivity;
+		orientationChanged = true;
+	}
+
+	if(orientationChanged)
+	{
+		// Clamp pitch to avoid flipping the camera
+		if (m_Pitch > 89.0f) m_Pitch = 89.0f;
+		if (m_Pitch < -89.0f) m_Pitch = -89.0f;
+
+		m_Orientation.x = cos(glm::radians(m_Yaw)) * cos(glm::radians(m_Pitch));
+		m_Orientation.y = sin(glm::radians(m_Pitch));
+		m_Orientation.z = sin(glm::radians(m_Yaw)) * cos(glm::radians(m_Pitch));
+		m_Orientation = glm::normalize(m_Orientation);
+
+		modified = true;
+	}
+
 	// Update matrices
 	if (modified)
 	{
-		const glm::mat4 viewMatrix = glm::lookAt(m_Position, m_Position + glm::vec3(0.0f, 0.0f, -1.0f), glm::vec3(0.0f, 1.0f, 0.0f));
-		const glm::mat4 projMatrix = glm::ortho(m_ZoomUnits * m_AspectRatio, m_Width - (m_ZoomUnits * m_AspectRatio), m_ZoomUnits, m_Height - m_ZoomUnits, -1.0f, 1.0f);
+		const glm::mat4 viewMatrix = glm::lookAt(m_Position, m_Position + m_Orientation, glm::vec3(0.0f, 1.0f, 0.0f));
+		const glm::mat4 projMatrix = glm::ortho(m_ZoomUnits * m_AspectRatio, m_Width - (m_ZoomUnits * m_AspectRatio), m_ZoomUnits, m_Height - m_ZoomUnits, Z_NEAR, Z_FAR);
 		m_CamMatrix = projMatrix * viewMatrix;
 	}
 }
--- OpenGLProject/src/Camera.h
+++ OpenGLProject/src/Camera.h
@@ -11,12 +11,21 @@ private:
 	static constexpr float m_ZoomSpeed = 1.0f;
 
 	glm::vec3 m_Position;
+	glm::vec3 m_Orientation, m_UpDirection;
 	float m_Width, m_Height;
 	float m_AspectRatio;
 
 	float m_ZoomUnits;
 
 	glm::mat4 m_CamMatrix;
+
+	// Euler angles for camera orientation
+	float m_Yaw = -90.0f; // Yaw is initialized facing -Z
+	float m_Pitch = 0.0f; // Pitch is initialized facing the horizon
+	const float m_Sensitivity = 0.1f; // for m_Orientation speed
+
+	static constexpr float Z_NEAR = 0.1f;
+	static constexpr float Z_FAR = 1000.0f;
 
 public:
 	Camera(glm::vec3 position, float width, float height);
